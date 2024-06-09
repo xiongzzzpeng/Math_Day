8dcf1a6fd5e37d23c23e26c7c48d07d6b807c122
PENG_TainHH <1319469766@qq.com>
2024-06-09 15:43:00 +0800

    updata

---
+++ 模板/快速幂.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define fs first
+#define sc second
+#define LOCAL
+typedef pair<int, int> PII;
+ll p;
+
+// 快速幂(a的b次幂)
+int qpow(ll a, ll b)
+{
+    ll ans = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            ans = ans * a % p;
+        a = a * a % p;
+        b >>= 1;
+    }
+    return ans;
+}
+
+void issue()
+{
+    ll a, b;
+    cin >> a >> b >> p;
+    cout << qpow(a, b) << endl;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+#ifdef LOCAL
+    freopen("xzp.in", "r", stdin);
+    freopen("xzp.out", "w", stdout);
+#endif
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        issue();
+    }
+    return 0;
+}
