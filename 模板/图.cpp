3a4876bb14da1672b7cb70b2affbedf10777dd32
PENG_TainHH <1319469766@qq.com>
2024-07-07 11:12:15 +0800

    update

--- 模板/图.cpp
+++ 模板/图.cpp
@@ -11,9 +11,15 @@ struct Use_map
 {
     vector<vector<int>> graph1; // 邻接矩阵
     vector<vector<PII>> graph2; // 邻接表
+    // 链式前向星
+    vector<int> head;   // 头指针,头边的编号
+    vector<int> nxt;    // 该点下一个移动的点
+    vector<int> to;     // 目标点
+    vector<int> weigth; // 边的权值
+    int cnt;            // 计数,从哪开始
 
     // 11是10个点1~11
-    Use_map() : graph1(11, vector<int>(11, 0))
+    Use_map() : graph1(11, vector<int>(11, 0)), head(11), nxt(11), to(11), weigth(11)
     {
     }
 
@@ -31,6 +37,19 @@ struct Use_map
         // 初始化邻接表
         graph2.clear();
         graph2.resize(n + 1); // 准备邻接表
+
+        // 初始化链式前向星
+        cnt = 1; // 从结点1开始
+        fill(head.begin(), head.end(), 0);
+    }
+
+    // 链式前向星加边
+    void addEdge(int u, int v, int w)
+    {
+        nxt[cnt] = head[u];
+        to[cnt] = v;
+        weigth[cnt] = w;
+        head[u] = cnt++;
     }
 
     // 建有向图，edges是边
@@ -46,6 +65,12 @@ struct Use_map
         for (auto &edge : edges)
         {
             graph2[edge[0]].push_back({edge[1], edge[2]}); // from:0 to:1 权值:2
+        }
+
+        // 建链式前向星
+        for (auto &edge : edges)
+        {
+            addEdge(edge[0], edge[1], edge[2]); // from:0 to:1 边权:2
         }
     }
 
@@ -65,6 +90,13 @@ struct Use_map
             graph2[edge[0]].push_back({edge[1], edge[2]}); // from:0 to:1 权值:2
             graph2[edge[1]].push_back({edge[0], edge[2]});
         }
+
+        // 建链式前向星
+        for (auto &edge : edges)
+        {
+            addEdge(edge[0], edge[1], edge[2]); // from:0 to:1 边权:2
+            addEdge(edge[1], edge[0], edge[2]);
+        }
     }
 
     // 遍历图
@@ -83,6 +115,14 @@ struct Use_map
         {
             for (auto &edge : graph2[i])
                 cout << edge.fs << "," << edge.sc << " "; // to:fs 边长:sc
+            cout << endl;
+        }
+
+        // 遍历链式前向星
+        for (int i = 1; i <= n; i++)
+        {
+            for (int ei = head[i]; ei > 0; ei = nxt[ei])
+                cout << to[ei] << "," << weigth[ei] << " "; // 边权
             cout << endl;
         }
     }
