1f70c5ca3286ae043c2ec03ed4a363a72344a393
PENG_TainHH <1319469766@qq.com>
2024-07-15 17:57:59 +0800

    update

---
+++ 模板/Dijkstar.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define fs first
+#define sc second
+#define LOCAL
+typedef pair<int, int> PII;
+
+// Dijkstra
+// 使用范围:有向图，边无负数
+// 求源点到每个点的最短路
+
+int dijkstar(vector<vector<int>> &times, int n, int s)
+{
+    // n图中点的数量，s是源点
+    vector<vector<PII>> graph(n + 1);
+    for (auto &edge : times)
+        graph[edge[0]].emplace_back(edge[1], edge[2]);
+
+    // 记录源点到每个点的距离,并且初始化
+    vector<int> distance(n + 1, INT_MAX);
+    distance[s] = 0;
+    // 每个点是否访问过
+    vector<bool> visited(n + 1, false);
+    // 小根堆,前点，后距离
+    auto cmp = [](PII &a, PII &b)
+    {
+        return a.sc > b.sc;
+    };
+    priority_queue<PII, vector<PII>, decltype(cmp)> heap(cmp);
+    // 源点添加
+    heap.emplace(s, 0);
+
+    while (!heap.empty())
+    {
+        int u = heap.top().fs;
+        heap.pop();
+
+        if (visited[u])
+            continue;
+        visited[u] = true;
+
+        for (auto &edge : graph[u])
+        {
+            int v = edge.fs;
+            int w = edge.sc;
+            if (!visited[v] && distance[u] + w < distance[v])
+            {
+                distance[v] = distance[u] + w;
+                heap.emplace(v, distance[v]);
+            }
+        }
+    }
+
+    int ans = INT_MIN;
+    for (int i = 1; i <= n; i++)
+    {
+        if (distance[i] == INT_MAX)
+            return -1;
+        ans = max(ans, distance[i]);
+    }
+    return ans;
+}
+
+void issue()
+{
+    int n, m, s;
+    cin >> n >> m >> s;
+    vector<vector<PII>> graph(n + 1);
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].emplace_back(v, w);
+    }
+
+    // 记录源点到每个点的距离,并且初始化
+    vector<int> distance(n + 1, INT_MAX);
+    distance[s] = 0;
+    // 每个点是否访问过
+    vector<bool> visited(n + 1, false);
+    // 小根堆,前点，后距离
+    auto cmp = [](PII &a, PII &b)
+    {
+        return a.sc > b.sc;
+    };
+    priority_queue<PII, vector<PII>, decltype(cmp)> heap(cmp);
+    // 源点添加
+    heap.emplace(s, 0);
+
+    while (!heap.empty())
+    {
+        int u = heap.top().fs;
+        heap.pop();
+
+        if (visited[u])
+            continue;
+        visited[u] = true;
+
+        for (auto &edge : graph[u])
+        {
+            int v = edge.fs;
+            int w = edge.sc;
+            if (!visited[v] && distance[u] + w < distance[v])
+            {
+                distance[v] = distance[u] + w;
+                heap.emplace(v, distance[v]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+        cout << distance[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+#ifdef LOCAL
+    freopen("xzp.in", "r", stdin);
+    freopen("xzp.out", "w", stdout);
+#endif
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        issue();
+    }
+    return 0;
+}
